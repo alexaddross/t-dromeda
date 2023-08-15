900a3988d3e5a3fad1fd7e0f4d5429ed0044d514
Alexander <alexaddros.s@yandex.ru>
2023-08-15 15:54:06 +0300

    initial auth-user data request system

--- core.py
+++ core.py
@@ -112,7 +112,7 @@ async def get_robot_data(body: TelegramAccount):
     with Session(memory_engine) as session:
         with Session(auth_engine) as auth:
             record = auth.query(TelegramID).where(TelegramID.group_id == body.telegram_id).one()
-            robot = auth.query(Robot).where(Robot.id == record.robot_id)
+            robot = auth.query(Robot).where(Robot.id == record.robot_id).one()
 
         result = session.query(RobotDataDB).where(RobotDataDB.serial==robot.serial).one()
     
